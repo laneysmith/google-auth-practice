80c6c0767676d9da04b902aa4afe9c35c7a4cbc1
Laney Smith <lsmith.unc@gmail.com>
2016-06-24 14:33:44 -0600

    Add passport

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('cookie-session');
+var auth = require('./auth');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -20,7 +22,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({keys: [process.env.SESSION_KEY1, process.env.SESSION_KEY2]}));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(auth.passport.initialize());
 
 app.use('/', routes);
 app.use('/users', users);
